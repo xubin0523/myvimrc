1e67da723a33b4f3875d4161eef94d6bc531d6dd
{许斌}({067532}) <mark.xub@alibaba-inc.com>
2015-03-22 20:46:30 +0800

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mkdir -p ~/.vim/colors && cd ~/.vim/colors
+wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
+
+#mkdir -p ~/.vim/autoload ~/.vim/bundle
+#curl -so ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+cd ~/.vim/bundle
+git clone git://github.com/Lokaltog/vim-powerline.git
+
+cd ~/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim.git
+
+sudo pip install jedi
+cd ~/.vim/bundle
+git clone git://github.com/davidhalter/jedi-vim.git
+git clone https://github.com/scrooloose/syntastic.git
+
+mkdir -p ~/.vim/ftplugin
+wget -O ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
